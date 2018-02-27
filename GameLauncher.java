b71796fabfb6f331cfa3e85f2b90079d9e75d818
Joe Hill <joe@jkh3.com>
2018-02-26 23:06:49 -0500

    Creating an automated Guessing Game in Java

---
+++ GameLauncher.java
@@ -0,0 +1,10 @@
+
+
+public class GameLauncher {
+    public static void main (String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
+
+//This is the code  you need to run after compiling the three java files. This will get you the results. 
